e3a18b68f9124cdede45108f55ed4d2b0c606286
Gao Zhigang CNLXR <gzg1984@aliyun.com>
2022-02-22 23:36:13 -0800

    first copy from eclips newb , cannot pass compile

---
+++ src/main/java/admin/IDao/projectMapper.java
@@ -0,0 +1,28 @@
+package admin.IDao;
+
+import admin.domain.project;
+import admin.domain.projectWithBLOBs;
+
+import java.util.List;
+
+public interface projectMapper {
+    int deleteByPrimaryKey(Integer projectId);
+
+    int insert(projectWithBLOBs record);
+
+    int insertSelective(projectWithBLOBs record);
+
+    projectWithBLOBs selectByPrimaryKey(Integer projectId);
+    
+    List<projectWithBLOBs> getAllprojects();
+
+    int updateByPrimaryKeySelective(projectWithBLOBs record);
+
+    int updateByPrimaryKeyWithBLOBs(projectWithBLOBs record);
+
+    int updateByPrimaryKey(project record);
+    
+    /* new for upload */
+    void addProjectBase(projectWithBLOBs po);
+
+}
---
+++ src/main/java/admin/web/projectService.java
@@ -0,0 +1,16 @@
+package admin.web;
+ 
+import admin.domain.project;
+import admin.domain.projectWithBLOBs;
+import java.util.ArrayList;
+import java.util.List;
+import java.sql.SQLException;
+
+public interface projectService {
+	public List<projectWithBLOBs> getAllprojects();
+	
+	/*2020 1103 for add project*/
+    void addProjectBase(projectWithBLOBs projectBasePO) throws SQLException;
+    void updateProjectBaseStatus(String projectId,String status) throws SQLException;
+
+}
